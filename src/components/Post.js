d2c5b4559a5303ca5b40004edd5065bb81dcaf25
natalyyemal <nyemal@udesa.edu.ar>
2021-11-03 01:06:55 -0300

    coment

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
+
 import React, { Component } from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import { auth, db } from '../firebase/config';
+import {Text, View, StyleSheet, TouchableOpacity, Modal, TextInput} from 'react-native';
+import { db, auth } from '../firebase/config';
 import firebase from 'firebase';
 
 class Post extends Component{
@@ -9,17 +10,20 @@ class Post extends Component{
         this.state = {
            likes: 0,
            myLike:false,
+           showModal: false, //Para la vista del modal
+           comment:'', //para limpiar el campo después de enviar.
         }
     }
     componentDidMount(){
-       if(this.props.postData.data.likes){
-           this.setState({
-               likes: this.props.postData.data.likes.length,
-               myLike: this.props.postData.data.likes.includes(auth.currentUser.email),
-            })
+        if(this.props.postData.data.likes){
+            this.setState({
+            likes:this.props.postData.data.likes.length,
+            myLike: this.props.postData.data.likes.includes(auth.currentUser.email),  
+        })
         }
+        
     }
-    
+
     darLike(){
         //Agregar mi usuario a un array de usuario que likearon.
             //Updatear el registro (documento)
@@ -28,44 +32,110 @@ class Post extends Component{
         })
         .then(()=>{
             this.setState({
-                likes: this.state.likes + 1,
+                likes:this.props.postData.data.likes.length,
+                //likes:this.state.likes + 1, //Opción más rápida de respuesta
                 myLike: true,
             })
-
         })
-        //Cambiar estado
     }
     quitarLike(){
-      //quitar mi usuario a un array de usuario que likearon.
+        //Quitar mi usuario a un array de usuario que likearon.
             //Updatear el registro (documento)
-            db.collection('posts').doc(this.props.postData.id).update({
-                likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+        db.collection('posts').doc(this.props.postData.id).update({
+            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+        })
+        .then(()=>{
+            this.setState({
+                likes:this.props.postData.data.likes.length,
+                //likes:this.state.likes + 1, //Opción más rápida de respuesta
+                myLike: false,
             })
-            .then(()=>{
-                this.setState({
-                    likes: this.props.postData.data.likes.length,
-                    myLike: false,
-                })
-    
-            })
-            //Cambiar estado
-        }   
+        })
+    }
+    showModal(){
+        this.setState({
+            showModal:true,
+        })
+    }
+
+    hideModal(){
+        this.setState({
+            showModal:false,
+        })
+    }
+
+    guardarComentario(){
+        console.log('Guardadno comentario...');
+        let oneComment = {
+            createdAt: Date.now(),
+            author: auth.currentUser.email,
+            comment: this.state.comment, 
+        }
+        //identifacar el documento que queremos modificar.
+         db.collection('posts').doc(this.props.postData.id).update({
+           comments:firebase.firestore.FieldValue.arrayUnion(oneComment)
+        })
+
+        //Armar el comentario que vamos a guardar.  
+            //Conseguir el estado
+
+        //Guardarlo en un colección: modifacer un documento
+
+        //limpiar el estado
+    }
+
     render(){
         console.log(this.props);
         return(
             <View style={styles.contanier}>
              <Text>Texto del post: {this.props.postData.data.texto}</Text>
              <Text>user: {this.props.postData.data.owner} </Text>  
-             <Text>Likes: {this.state.likes} </Text> 
-             {
-             this.state.myLike == false?
-             <TouchableOpacity onPress={()=>this.darLike()}>
-                 <Text>Me gusta</Text>
-             </TouchableOpacity> :
-             <TouchableOpacity onPress={()=>this.quitarLike()}>
-             <Text>Quitar like</Text>
-         </TouchableOpacity> 
-    }         
+            <Text>Likes: {this.state.likes} </Text>  
+             {/* Cambio de botones me gusta/ me dejó de gustar */}
+            {
+                this.state.myLike == false ?
+                <TouchableOpacity onPress={()=>this.darLike()}>
+                    <Text>Me gusta</Text>
+                </TouchableOpacity> :
+                <TouchableOpacity onPress={()=>this.quitarLike()}>
+                    <Text>Quitar like</Text>
+                </TouchableOpacity>                       
+            }
+            {/* Ver modal */}
+            <TouchableOpacity onPress={()=>this.showModal()}>
+                <Text>Ver Comentarios</Text>
+            </TouchableOpacity>
+
+            {/* Modal para comentarios */}
+            {   this.state.showModal ?
+                <Modal
+                    visible={this.state.showModal}
+                    animationType='slide'
+                    transparent={false}
+                >   
+                    <TouchableOpacity onPress={()=>this.hideModal()}>
+                        <Text>X</Text>
+                    </TouchableOpacity> 
+                    <Text>Dentro del modal</Text>
+
+                    {/* Formulario para nuevo comentarios */}
+                    <View>
+                        <TextInput placeholder="Comentar..."
+                            keyboardType="default"
+                            multiline
+                            onChangeText={text => this.setState({comment: text})}
+                            value={this.state.comment}
+                        />
+                        <TouchableOpacity onPress={()=>{this.guardarComentario()}}>
+                            <Text>Guadar comentario</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </Modal>    :
+                <Text></Text>
+            } 
+
+
             </View>
         )
     }
@@ -83,4 +153,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Post;
+export default Post
