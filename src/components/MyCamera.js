25265caf9587a18dada612a9ccff8a54616d43f2
calistaortiz <63122831+calistaortiz@users.noreply.github.com>
2021-11-08 11:41:51 -0300

    camara lista

--- src/components/MyCamera.js
+++ src/components/MyCamera.js
@@ -1,48 +1,96 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
-import { Camera } from 'expo-camera'
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import {storage} from '../firebase/config';
 
 class MyCamera extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             permission: false,
             photo: "",
-        }
+        };
         this.camera;
     }
 
+     componentDidMount(){
+         Camera.requestCameraPermissionAsync()
+          .then(() =>{
+              this.setState({
+                permission: true,
+              });
+         })
+        .catch((err)=> console.log(err));
+        
+
+    Camera.getAvailableCameraTypesAsync().then((res)=> console.log(res));
+        }
+
     takePicture() {
         //cuando se ejecuta este metodo devuelve una promesa
-        this.camera.takePictureAsync()
+        this.camera
+        .takePictureAsync()
             .then((photo) => {
                 console.log(photo);
                 this.setState({
                     //uri es donde se almacena la foto, nosotros no lo vemos
-
                     photo: photo.uri,
                 });
             })
-            .catch((err) => console.log(err))
+            .catch((err) => console.log(err));
     }
 
-    render() {
-        return (
-            <>
-                <Camera
-                    style={{ flex: 1, width: "100%" }}
-                    type={Camera.Constants.Type.front}
-                    ref={(cam) => (this.camera = cam)}
-                />
-                <TouchableOpacity onPress={() => this.takePicture()}>
-                    <Text>Shoot</Text>
-                </TouchableOpacity>
-            </>
-        );
+    savePhoto() {
+        //console.log("guardar foto en firebase");
+        fetch(this.state.photo)
+            .then((res) => res.blob())
+            .then((image) => {
+                const ref = storage.ref(`photos/${Date.now()}.jpg`);
+
+                ref.put(image).then(() => {
+                    ref
+                        .getDownloadURL()
+                        .then((url) => console.log("guardar imagenn en la base de datos"));
+                });
+            })
+            .catch((err) => console.log(err));
+    }
+
+render() {
+    return (
+        <>
+            {this.state.photo ? (
+                <>
+                    <Image
+                        style={{ flex: 1, width: "100%" }}
+                        source={{ uri: this.state.photo }}
+                    />
+                    <View>
+                        <TouchableOpacity onPress={() => this.savePhoto()}>
+                            <Text>Aceptar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text>Cancelar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </>
+            ) : (
+                    <>
+                        <Camera
+                            style={{ flex: 1, width: "100%" }}
+                            type={Camera.Constants.Type.front}
+                            ref={(cam) => (this.camera = cam)}
+                        />
+                        <TouchableOpacity onPress={() => this.takePicture()}>
+                            <Text>Shoot</Text>
+                        </TouchableOpacity>
+                    </>
+                )}
+        </>
+    );
     }
 }
-
 const styles = StyleSheet.create({})
 
 
