e07032f38f5eecf66a98469c4f757302c62f3502
calistaortiz <63122831+calistaortiz@users.noreply.github.com>
2021-11-08 10:37:23 -0300

    vol2

--- src/components/MyCamera.js
+++ src/components/MyCamera.js
@@ -1,50 +1,49 @@
-import { Camera } from 'expo-camera';
-import React, {Component} from 'react'
-import {Text, View, TouchableOpacity} from 'react-native';
-
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Camera } from 'expo-camera'
 
 class MyCamera extends Component {
 
-    constructor (props){
-        super(props);
-        this.state={
+    constructor(props) {
+        super(props)
+        this.state = {
             permission: false,
             photo: "",
         }
         this.camera;
     }
-    takePicture(){
+
+    takePicture() {
+        //cuando se ejecuta este metodo devuelve una promesa
         this.camera.takePictureAsync()
-        .then((photo) => {
-            console.log (photo)
-            this.setState({
-                photo: photo.uri,
+            .then((photo) => {
+                console.log(photo);
+                this.setState({
+                    //uri es donde se almacena la foto, nosotros no lo vemos
 
+                    photo: photo.uri,
+                });
             })
-
-
-        })
-        .catch((err) => console.log(err))
-
+            .catch((err) => console.log(err))
     }
+
     render() {
         return (
-        <> 
-        <Camera
-            style={{ flex: 1, width: "100%" }}
-            type={Camera.Constants.Type.front}
-            ref={(cam) => (this.camera = cam)}
-        />
-         <TouchableOpacity onPress={()=> this.takePicture()}> 
-             <Text> Shoot</Text>
-            </TouchableOpacity>
-       </>
-     );
+            <>
+                <Camera
+                    style={{ flex: 1, width: "100%" }}
+                    type={Camera.Constants.Type.front}
+                    ref={(cam) => (this.camera = cam)}
+                />
+                <TouchableOpacity onPress={() => this.takePicture()}>
+                    <Text>Shoot</Text>
+                </TouchableOpacity>
+            </>
+        );
     }
 }
 
-
-           
+const styles = StyleSheet.create({})
 
 
 
