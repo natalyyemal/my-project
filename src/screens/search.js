bbbc7b488faaa465c85376f617e924a1def8559b
calistaortiz <63122831+calistaortiz@users.noreply.github.com>
2021-11-18 10:48:43 -0300

    search

--- src/screens/search.js
+++ src/screens/search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import React from 'react'
-import {View, Button, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Button, Text, TextInput, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 
 class Search extends Component{
     constructor(props){
@@ -14,17 +14,53 @@ class Search extends Component{
     componentDidUpdate(){
         this.todosLosPosteos();
     }
+    todosLosPosteos(){
+        db.collection ('posts'). where ('titile', '?=', this.state.search).onSnapshot(
+          docs => {
+              let posts = [];
+              docs.forEach( doc =>{
+                  posts.push({
+                      id:doc.id,
+                      data:doc.data()
+                  })
+                  this.setState({
+                      todosLosPosteos: posts,
+                  })
+              })
+          }  
+        )
+    }
 
     render(){
         return(
+            <View style={StyleSheet.container}>
+                <TextInput
+                onChangeText={(text) => this.setState({search: text})}
+                placeholder="buscar"
+                keyboeardType="default"
+                multiline value= {this.state.search}
+               />
+             <Button 
+             title="buscar"
+             onPress={() => this.todosLosPosteos}>
 
+             </Button>
+             <FlatList 
+             data={this.state.posteos}
+             keyExtractor={item => item.id.toString()}
+             renderItem= {({item}) => <Posteo data = {item}/>}
+             />
+
+            </View>
         )
     }
 }
 
-
-
-
-
 export default Search
 
+const styles =StyleSheet.create({
+    container: {
+        felx: 1,
+        backgroundColor: 'white',
+    }
+})
